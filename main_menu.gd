6557868716402d4f0399fbe784962ce3faaf3cdc
bonzelett-ux <bonzelett@gmail.com>
2025-09-13 17:07:12 -0500

    Main Menu

--- main_menu.gd
+++ main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://Game.tscn")
+	get_tree().change_scene_to_file("res://player.tscn")
 	
 func _on_quit_pressed():
 	get_tree().quit()
