41de76d3e4b0042edd531ff585b98778285b036e
bonzelett-ux <bonzelett@gmail.com>
2025-09-14 00:42:15 -0500

    LEVEL222222

---
+++ goal.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+@export var next_scene_path: String = "res://Level2.tscn"
+
+func _ready() -> void:
+	connect("body_entered", Callable(self, "_on_body_entered"))
+
+func _on_body_entered(body: Node) -> void:
+	print("Entered goal:", body.name, " class:", body.get_class())
+	if body.is_in_group("player"):
+		print("Level Complete!")
+		await get_tree().create_timer(1.0).timeout
+		get_tree().change_scene_to_file(next_scene_path)
